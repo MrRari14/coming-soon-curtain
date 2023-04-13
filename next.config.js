26891836857dc8033f20257e2ec0e12b65d2e719
MrRari14 <ber.97ferrari@gmail.com>
2023-04-13 15:04:23 -0300

    ✨ Implementing security headers

--- next.config.js
+++ next.config.js
@@ -4,3 +4,66 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// Security HTTP headers
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`
+const securityHeaders = [
+  // X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  // Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  // X-XSS-Protection
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  // X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  // Permissions-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'
+  },
+  // X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  // Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  },
+  // Content-Security-Policy
+  // {
+  //   key: 'Content-Security-Policy',
+  //   value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+  // }
+]
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
